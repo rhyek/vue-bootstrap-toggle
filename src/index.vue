e88de9f16d6a75390222287966d2b14f74f9849d
Carlos Gonzales <carlos.rgn@gmail.com>
2017-07-19 18:10:55 -0600

    export defaults. closes #4

--- src/index.vue
+++ src/index.vue
@@ -3,42 +3,46 @@
 </template>
 
 <script>
-  if (!jQuery().bootstrapToggle) {
-    require('bootstrap-toggle')
-  }
+import merge from 'merge'
 
-  export default {
-    props: {
-      value: Boolean,
-      options: {
-        type: Object,
-        default: () => ({})
-      }
-    },
-    computed: {
-      $$el() {
-        return jQuery(this.$el)
-      }
-    },
-    watch: {
-      value(newValue) {
-        this.$$el.bootstrapToggle(newValue ? 'on' : 'off')
-      }
-    },
-    mounted() {
-      if (this.value) {
-        this.$el.checked = true
-      }
-      this.$$el.bootstrapToggle(this.options)
-      this.$$el.change(() => {
-        this.$emit('input', this.$$el.prop('checked'))
-      })
-    },
-    beforeDestroy() {
-      this.$$el.bootstrapToggle('destroy')
-      this.$$el.off('change')
+export const defaults = {}
+
+if (!jQuery().bootstrapToggle) {
+  require('bootstrap-toggle')
+}
+
+export default {
+  props: {
+    value: Boolean,
+    options: {
+      type: Object,
+      default: () => ({})
     }
+  },
+  computed: {
+    $$el() {
+      return jQuery(this.$el)
+    }
+  },
+  watch: {
+    value(newValue) {
+      this.$$el.bootstrapToggle(newValue ? 'on' : 'off')
+    }
+  },
+  mounted() {
+    if (this.value) {
+      this.$el.checked = true
+    }
+    this.$$el.bootstrapToggle(merge.recursive(true, defaults, this.options))
+    this.$$el.change(() => {
+      this.$emit('input', this.$$el.prop('checked'))
+    })
+  },
+  beforeDestroy() {
+    this.$$el.bootstrapToggle('destroy')
+    this.$$el.off('change')
   }
+}
 </script>
 
 <style src="bootstrap-toggle/css/bootstrap-toggle.css"/>
